e0a0e9f9971b13eb92090b40397a7f7a915e8793
Brad Parker <hi@bradparker.com>
2022-09-29 13:09:53 +1000

    Pin dev nixpkgs ref

--- shell.nix
+++ shell.nix
@@ -1,6 +1,9 @@
 let
-  nixpkgs = import <nixpkgs> {};
-  haskellPackages = nixpkgs.haskell.packages.ghc921;
+  nixpkgs-source = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/7d226b5d12965b214d299d553fff6a3db7e4202a.tar.gz";
+  };
+  nixpkgs = import nixpkgs-source {};
+  haskellPackages = nixpkgs.haskell.packages.ghc924;
   package = import ./. { haskellPackages = haskellPackages; };
 in
   haskellPackages.shellFor {
